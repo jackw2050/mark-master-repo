0944ed258a1356cb8c43d16d04a32b8d100ad828
Matt White <mattwhite614@gmail.com>
2016-08-09 20:31:53 -0500

    deploy beta

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mysql://fojhoi9rtl9krnvu:inqvhbvitht8xj2i@gx97kbnhgjzh3efb.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jv4mpuj72z8hl6kt' 
+
+};
